df3450e96e9316a4e0ded4a5a4a192db07ba86d6
Airi Nakama <62741497+milky-airi@users.noreply.github.com>
2023-08-02 10:34:18 +0000

    Ouyou Kadai 7a answer

--- bookers2-practice/bookers2-Ouyou-Kadai-7a/app/controllers/books_controller.rb
+++ bookers2-practice/bookers2-Ouyou-Kadai-7a/app/controllers/books_controller.rb
@@ -12,10 +12,16 @@ class BooksController < ApplicationController
     @book = Book.new
     to = Time.current.at_end_of_day
     from = (to - 6.day).at_beginning_of_day
-    @books = Book.includes(:favorited_users).
-      sort_by {|x|
-        x.favorited_users.includes(:favorites).where(created_at: from...to).size
-      }.reverse
+    # @books = Book.includes(:favorited_users).
+    #   sort_by {|x|
+    #     x.favorited_users.includes(:favorites).where(created_at: from...to).size
+    #   }.reverse
+
+    @books = Book.all.sort {|a,b|
+      b.favorites.where(created_at: from...to).size <=>
+      a.favorites.where(created_at: from...to).size
+    }
+
   end
 
   def create
